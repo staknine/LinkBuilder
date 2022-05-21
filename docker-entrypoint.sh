140e2f648bf98fd2bc5a8fc7ac4db3f6b281530b
StakNine <staknine@protonmail.com>
2022-05-21 14:32:19 -0700

    Remove generated docker & deploy files

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Run the migration first using the custom release task
-bin/link_builder eval "Release.migrate"
-
-# Launch the OTP release and replace the caller as Process #1 in the container
-exec bin/link_builder "$@"
